91e6fad5d9fe4407e3ead16e1c0d3607586e309c
barriosnahuel <barrios.nahuel@gmail.com>
2012-09-28 18:01:47 -0300

    Improve coding conventions and fix warnings. There's still one refactor to do.

--- src/main/java/com/nbempire/android/magicannotator/util/android/view/ScoreEditorView.java
+++ src/main/java/com/nbempire/android/magicannotator/util/android/view/ScoreEditorView.java
@@ -17,7 +17,9 @@ import android.widget.TextView;
 import com.nbempire.android.magicannotator.R;
 
 /**
- * TODO : JavaDoc : for ScoreEditorView.
+ * GUI entity type to use with the {@code scoreeditor_horizontal.xml} layout because it does its functionality. User can use it from layout
+ * resources with or without setting the {@code increment} XML attribute. If you don't pass that attribute, its default value will be {@code
+ * 1}.
  * <p/>
  * Use example in XML layout file:
  * <p/>
@@ -28,6 +30,7 @@ import com.nbempire.android.magicannotator.R;
  * &lt;com.nbempire.android.magicannotator.util.android.view.ScoreEditorView android:id="@+id/anId" increment="2" /&gt;
  *
  * @author Nahuel Barrios.
+ * @since 1
  */
 public class ScoreEditorView extends RelativeLayout {
 
@@ -40,7 +43,9 @@ public class ScoreEditorView extends RelativeLayout {
      * A constructor method for the {@link ScoreEditorView} type.
      *
      * @param context
+     *         The view's context.
      * @param playerNickname
+     *         The name of the player linked with this ScoreEditorView.
      */
     public ScoreEditorView(Context context, String playerNickname) {
         super(context);
@@ -49,25 +54,37 @@ public class ScoreEditorView extends RelativeLayout {
     }
 
     /**
-     * TODO : JavaDoc : for ScoreEditorView.initializeView().
+     * A constructor method for the {@link ScoreEditorView} type receiving XML attributes from the layout.
      *
      * @param context
+     *         The view's context.
      * @param attributes
+     *         {@link AttributeSet} containing the XML attributes used defining the layout.
+     *         <p/>
+     *         E.g.:
+     *         <p/>
+     *         &lt;com.nbempire.android.magicannotator.util.android.view.ScoreEditorView android:id="@+id/anId" increment="2" /&gt;
+     *         <p/>
+     *         So if you do {@code attributes.getAttributeIntValue(null, increment, increment);} you wil get {@code 2}.
+     *         <p/>
+     *         Please note that the second parameter has to be the String literal {@code increment}.
      */
     private void initializeView(Context context, AttributeSet attributes) {
         int increment = 1;
         if (attributes != null) {
-            attributes.getAttributeIntValue(null, "increment", increment);
+            increment = attributes.getAttributeIntValue(null, "increment", increment);
         }
 
         initializeView(context, increment);
     }
 
     /**
-     * TODO : JavaDoc : for ScoreEditorView.initializeView().
+     * A constructor method for the {@link ScoreEditorView} type.
      *
      * @param context
+     *         The view's context.
      * @param increment
+     *         The increment used to add/substract scores.
      */
     private void initializeView(Context context, int increment) {
         LayoutInflater.from(context).inflate(R.layout.scoreeditor_horizontal, this, true);
@@ -83,9 +100,10 @@ public class ScoreEditorView extends RelativeLayout {
     }
 
     /**
-     * TODO : JavaDoc : for ScoreEditorView.addOnTouchActions().
+     * Add corresponding functionality to add/substract buttons when users touch the screen.
      *
      * @param increment
+     *         The increment used to add/substract scores.
      */
     public void addOnTouchActions(int increment) {
         Button plus = (Button) findViewById(R.id.plusButton);
@@ -96,41 +114,37 @@ public class ScoreEditorView extends RelativeLayout {
     }
 
     /**
-     * TODO : JavaDoc : for ScoreEditorOnTouchListener. Pasar esta clase afuera y ver de refactorear la del truco pudiendole pasar a esta un
-     * validador.
+     * Listener that implements {@link OnTouchListener} to do the functionality to add/substract score.
+     * <p/>
+     * TODO : Refactor : Esta clase afuera y que el Truco use esta con algún validador.
      *
      * @author Nahuel Barrios.
+     * @since 1
      */
     class ScoreEditorOnTouchListener implements OnTouchListener {
 
-        private boolean forSubstract = true;
+        private boolean forSubstract = false;
 
         private int increment = 1;
 
         /**
          * A constructor method for the {@link ScoreEditorOnTouchListener} type.
-         */
-        public ScoreEditorOnTouchListener() {
-            super();
-            this.forSubstract = false;
-        }
-
-        /**
-         * A constructor method for the {@link ScoreEditorOnTouchListener} type.
          *
          * @param increment
+         *         The increment used to add scores.
          */
         public ScoreEditorOnTouchListener(int increment) {
             super();
             this.increment = increment;
-            this.forSubstract = false;
         }
 
         /**
          * A constructor method for the {@link ScoreEditorOnTouchListener} type.
          *
          * @param increment
+         *         The increment used to add/substract scores depending on the value of {@code forSubstract} parameter.
          * @param forSubstract
+         *         Boolean value indicating with {@code true} when the listener has to substract, or {@code false} for add.
          */
         public ScoreEditorOnTouchListener(int increment, boolean forSubstract) {
             super();
@@ -158,14 +172,19 @@ public class ScoreEditorView extends RelativeLayout {
     }
 
     /**
-     * Actualiza el valor original en base al incremento que le pasamos. TODO : JavaDoc : for ScoreEditorView.updateScore().
+     * Add value of {@code increment} to the current value of the specified {@code editText}.
+     * <p/>
+     * Please note that this method always add the {@code increment} value to the current value, so if you want to substract you will
+     * transform {@code increment} to the negative form.
      *
      * @param editText
-     * @param value
+     *         The editText to update.
+     * @param increment
+     *         The increment used to add scores.
      */
-    private void updateScore(EditText editText, int value) {
+    private void updateScore(EditText editText, int increment) {
         String currentValue = editText.getText().toString();
-        editText.setText(Integer.toString(Integer.parseInt(currentValue) + value));
+        editText.setText(Integer.toString(Integer.parseInt(currentValue) + increment));
     }
 
 }
