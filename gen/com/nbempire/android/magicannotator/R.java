1c63efd1fb54345f3f788b457fe20e8731cd20f2
Nahuel Barrios <barrios.nahuel@gmail.com>
2012-12-07 22:38:36 -0300

    Update android:versionCode="15" android:versionName="1.6"

--- gen/com/nbempire/android/magicannotator/R.java
+++ gen/com/nbempire/android/magicannotator/R.java
@@ -214,6 +214,11 @@ public final class R {
          * Base application theme for API 11+. This theme completely replaces AppBaseTheme from res/values/styles.xml on API 11+ devices.
          * <p/>
          * API 11 theme customizations can go here.
+         * <p/>
+         * Base application theme for API 14+. This theme completely replaces AppBaseTheme from BOTH res/values/styles.xml and
+         * res/values-v11/styles.xml on API 14+ devices.
+         * <p/>
+         * API 14 theme customizations can go here.
          */
         public static final int AppBaseTheme = 0x7f060000;
         /**
