c0460e3e77e30a233b9eedc7789544d39099b7ef
Nahuel Barrios <barrios.nahuel@gmail.com>
2012-12-07 22:32:56 -0300

    Add special styles.xml for API 11 and 14 to give user a better usage experience merged with the entire OS.

--- gen/com/nbempire/android/magicannotator/R.java
+++ gen/com/nbempire/android/magicannotator/R.java
@@ -20,66 +20,66 @@ public final class R {
     }
 
     public static final class id {
-        public static final int about_contact = 0x7f070003;
-        public static final int about_greetings = 0x7f070005;
-        public static final int about_introduction = 0x7f070001;
-        public static final int about_permissions = 0x7f070002;
-        public static final int about_projectUrl = 0x7f070004;
-        public static final int about_title = 0x7f070000;
-        public static final int basescrollview_headerText = 0x7f070007;
-        public static final int basescrollview_tableLayout = 0x7f070006;
-        public static final int button1 = 0x7f07002f;
-        public static final int button3 = 0x7f070033;
-        public static final int chanchoAnnotator_chooseLooserButton = 0x7f070009;
-        public static final int chanchoAnnotator_playersGridView = 0x7f070008;
-        public static final int choosePlayersMenuItem_deleteAll = 0x7f070037;
-        public static final int choosePlayers_button_makeTeams = 0x7f070010;
-        public static final int choosePlayers_newPlayer_button = 0x7f07000f;
-        public static final int choosePlayers_playersLayout = 0x7f070012;
-        public static final int gridView_cell_header = 0x7f070013;
-        public static final int gridView_cell_valuesLarge = 0x7f070016;
-        public static final int gridView_cell_valuesMedium = 0x7f070015;
-        public static final int gridView_cell_valuesSmall = 0x7f070014;
-        public static final int linearLayout1 = 0x7f07002e;
-        public static final int linearLayout3 = 0x7f070032;
-        public static final int main_bottomToolbarLayout = 0x7f07000c;
-        public static final int main_button1 = 0x7f07000d;
-        public static final int main_gamesListView = 0x7f07000b;
-        public static final int main_title = 0x7f07000a;
-        public static final int marketAnnotatorMenuItem_checkAllItems = 0x7f070038;
-        public static final int marketAnnotatorMenuItem_deleteAll = 0x7f07003b;
-        public static final int marketAnnotatorMenuItem_uncheckAllItems = 0x7f070039;
-        public static final int marketAnnotatorMenuItem_updateQuantitiesToZero = 0x7f07003a;
-        public static final int marketAnnotator_addButton = 0x7f07001d;
-        public static final int marketAnnotator_headerText = 0x7f07001b;
-        public static final int marketAnnotator_itemsLayout = 0x7f07001e;
-        public static final int marketAnnotator_scrollView = 0x7f07001c;
-        public static final int marketItem_addButton = 0x7f070019;
-        public static final int marketItem_itemCount = 0x7f070018;
-        public static final int marketItem_itemName = 0x7f070017;
-        public static final int marketItem_substractButton = 0x7f07001a;
-        public static final int playerNickname = 0x7f07001f;
-        public static final int plusButton = 0x7f070021;
-        public static final int score = 0x7f070020;
-        public static final int scrollView1 = 0x7f070011;
-        public static final int substractButton = 0x7f070022;
-        public static final int textView1 = 0x7f07000e;
-        public static final int trucoAnnotator_buttonNewGame = 0x7f07002c;
-        public static final int trucoAnnotator_labelDoTap = 0x7f070023;
-        public static final int trucoAnnotator_labelResult = 0x7f07002d;
-        public static final int trucoAnnotator_labelTeam1 = 0x7f070025;
-        public static final int trucoAnnotator_labelTeam2 = 0x7f070029;
-        public static final int trucoAnnotator_layoutTeam1 = 0x7f070024;
-        public static final int trucoAnnotator_layoutTeam2 = 0x7f070028;
-        public static final int trucoAnnotator_scoreTeam1 = 0x7f070026;
-        public static final int trucoAnnotator_scoreTeam2 = 0x7f07002a;
-        public static final int trucoAnnotator_substractButtonTeam1 = 0x7f070027;
-        public static final int trucoAnnotator_substractButtonTeam2 = 0x7f07002b;
-        public static final int tuteAnnotator_partialResults_gridView = 0x7f070031;
-        public static final int tutePartialResults_addTuteButton = 0x7f070030;
-        public static final int viewTeams_title = 0x7f070035;
-        public static final int viewteams_button_play = 0x7f070034;
-        public static final int whileLine1 = 0x7f070036;
+        public static final int about_contact = 0x7f080003;
+        public static final int about_greetings = 0x7f080005;
+        public static final int about_introduction = 0x7f080001;
+        public static final int about_permissions = 0x7f080002;
+        public static final int about_projectUrl = 0x7f080004;
+        public static final int about_title = 0x7f080000;
+        public static final int basescrollview_headerText = 0x7f080007;
+        public static final int basescrollview_tableLayout = 0x7f080006;
+        public static final int button1 = 0x7f08002f;
+        public static final int button3 = 0x7f080033;
+        public static final int chanchoAnnotator_chooseLooserButton = 0x7f080009;
+        public static final int chanchoAnnotator_playersGridView = 0x7f080008;
+        public static final int choosePlayersMenuItem_deleteAll = 0x7f080037;
+        public static final int choosePlayers_button_makeTeams = 0x7f080010;
+        public static final int choosePlayers_newPlayer_button = 0x7f08000f;
+        public static final int choosePlayers_playersLayout = 0x7f080012;
+        public static final int gridView_cell_header = 0x7f080013;
+        public static final int gridView_cell_valuesLarge = 0x7f080016;
+        public static final int gridView_cell_valuesMedium = 0x7f080015;
+        public static final int gridView_cell_valuesSmall = 0x7f080014;
+        public static final int linearLayout1 = 0x7f08002e;
+        public static final int linearLayout3 = 0x7f080032;
+        public static final int main_bottomToolbarLayout = 0x7f08000c;
+        public static final int main_button1 = 0x7f08000d;
+        public static final int main_gamesListView = 0x7f08000b;
+        public static final int main_title = 0x7f08000a;
+        public static final int marketAnnotatorMenuItem_checkAllItems = 0x7f080038;
+        public static final int marketAnnotatorMenuItem_deleteAll = 0x7f08003b;
+        public static final int marketAnnotatorMenuItem_uncheckAllItems = 0x7f080039;
+        public static final int marketAnnotatorMenuItem_updateQuantitiesToZero = 0x7f08003a;
+        public static final int marketAnnotator_addButton = 0x7f08001d;
+        public static final int marketAnnotator_headerText = 0x7f08001b;
+        public static final int marketAnnotator_itemsLayout = 0x7f08001e;
+        public static final int marketAnnotator_scrollView = 0x7f08001c;
+        public static final int marketItem_addButton = 0x7f080019;
+        public static final int marketItem_itemCount = 0x7f080018;
+        public static final int marketItem_itemName = 0x7f080017;
+        public static final int marketItem_substractButton = 0x7f08001a;
+        public static final int playerNickname = 0x7f08001f;
+        public static final int plusButton = 0x7f080021;
+        public static final int score = 0x7f080020;
+        public static final int scrollView1 = 0x7f080011;
+        public static final int substractButton = 0x7f080022;
+        public static final int textView1 = 0x7f08000e;
+        public static final int trucoAnnotator_buttonNewGame = 0x7f08002c;
+        public static final int trucoAnnotator_labelDoTap = 0x7f080023;
+        public static final int trucoAnnotator_labelResult = 0x7f08002d;
+        public static final int trucoAnnotator_labelTeam1 = 0x7f080025;
+        public static final int trucoAnnotator_labelTeam2 = 0x7f080029;
+        public static final int trucoAnnotator_layoutTeam1 = 0x7f080024;
+        public static final int trucoAnnotator_layoutTeam2 = 0x7f080028;
+        public static final int trucoAnnotator_scoreTeam1 = 0x7f080026;
+        public static final int trucoAnnotator_scoreTeam2 = 0x7f08002a;
+        public static final int trucoAnnotator_substractButtonTeam1 = 0x7f080027;
+        public static final int trucoAnnotator_substractButtonTeam2 = 0x7f08002b;
+        public static final int tuteAnnotator_partialResults_gridView = 0x7f080031;
+        public static final int tutePartialResults_addTuteButton = 0x7f080030;
+        public static final int viewTeams_title = 0x7f080035;
+        public static final int viewteams_button_play = 0x7f080034;
+        public static final int whileLine1 = 0x7f080036;
     }
 
     public static final class layout {
@@ -98,8 +98,8 @@ public final class R {
     }
 
     public static final class menu {
-        public static final int chooseplayers = 0x7f060000;
-        public static final int marketannotator = 0x7f060001;
+        public static final int chooseplayers = 0x7f070000;
+        public static final int marketannotator = 0x7f070001;
     }
 
     public static final class string {
@@ -199,5 +199,26 @@ public final class R {
          * ViewTeamsActivity
          */
         public static final int viewTeams_title = 0x7f040026;
+    }
+
+    public static final class style {
+        /**
+         * Base application theme, dependent on API level. This theme is replaced by AppBaseTheme from res/values-vXX/styles.xml on newer
+         * devices.
+         * <p/>
+         * <p/>
+         * Theme customizations available in newer API levels can go in res/values-vXX/styles.xml, while customizations related to
+         * backward-compatibility can go here.
+         * <p/>
+         * <p/>
+         * Base application theme for API 11+. This theme completely replaces AppBaseTheme from res/values/styles.xml on API 11+ devices.
+         * <p/>
+         * API 11 theme customizations can go here.
+         */
+        public static final int AppBaseTheme = 0x7f060000;
+        /**
+         * Application theme. All customizations that are NOT specific to a particular API-level can go here.
+         */
+        public static final int AppTheme = 0x7f060001;
     }
 }
