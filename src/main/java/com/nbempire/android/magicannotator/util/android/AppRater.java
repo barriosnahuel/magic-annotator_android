a9bd2188a7b6e29b97f704e30fcbf36cf58f3be3
Nahuel Barrios <barrios.nahuel@gmail.com>
2013-04-11 00:24:55 -0300

    Refactor of AppRater.java and also add functionality to track events to Google Analytics about user's selection when prompt is displayed; Fix Google Analytics key deletion.

--- src/main/java/com/nbempire/android/magicannotator/util/android/AppRater.java
+++ src/main/java/com/nbempire/android/magicannotator/util/android/AppRater.java
@@ -29,11 +29,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 import com.nbempire.android.magicannotator.AppParameter;
 import com.nbempire.android.magicannotator.R;
+import com.nbempire.android.magicannotator.util.android.analytics.GoogleAnalyticsUtil;
 
 /**
- * TODO : Javadoc for AppRater
+ * Utility type that displays a dialog to let user rate the application.
+ * <p/>
+ * It saves user preferences as {@link SharedPreferences} to handle when the dialog has to be opened or not.
+ * <p/>
+ * It also tracks events to Google Analytics about which is the user choice.
  * <p/>
  * Created on 4/9/13, at 8:47 PM.
  *
@@ -47,25 +53,36 @@ public class AppRater {
      */
     private static final String TAG = "AppRater";
 
-    private final static String APP_PACKAGE_NAME = "com.nbempire.android.magicannotator";
-
-    private static final String DONT_SHOW_DIALOG_AGAIN = "dontShowDialogAgain";
-
-    private static Dialog dialog;
-
-    private static SharedPreferences.Editor preferencesEditor;
+    /**
+     * Page name for Google Analytics for this type.
+     */
+    private static final String GOOGLE_ANALYTICS_PAGE_NAME = GoogleAnalyticsUtil.generatePageName(TAG);
 
     /**
-     * TODO : Javadoc for showRateDialogWhenCorresponding
+     * Key flag to use with the SharedPreferences.
+     */
+    private static final String DONT_SHOW_DIALOG_AGAIN = "dontShowDialogAgain";
+
+    private static Dialog s_dialog;
+
+    /**
+     * Key to use when tracking an event to Google Analytics. It must be used to track which button users selects when the dialog is displayed.
+     */
+    private static final String GOOGLE_ANALYTICS_EVENT_KEY = "Selected action on rateApp dialog";
+
+    /**
+     * Display an AlertDialog letting user choose between navigate to Android PlayStore to rate the app, remind him later, or disable prompt for
+     * ever.
      *
      * @param context
+     *         Activity's context.
      */
     public static void showRateDialogWhenCorresponding(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences("AppRater", Context.MODE_PRIVATE);
 
         boolean showDialog = !sharedPreferences.getBoolean(DONT_SHOW_DIALOG_AGAIN, false);
         if (showDialog) {
-            preferencesEditor = sharedPreferences.edit();
+            SharedPreferences.Editor preferencesEditor = sharedPreferences.edit();
 
             // Increment launch counter
             final String preferenceKeyAppRunCounter = "appRunCounter";
@@ -93,68 +110,123 @@ public class AppRater {
     }
 
     /**
-     * TODO : Javadoc for showRateDialog
+     * Create and display the dialog.
      * <p/>
      * This method can be called from any Activity with {@code (this, null)} parameters to display dialog at any time.
      *
      * @param context
-     * @param preferencesEditor
+     *         Activity's context.
      */
-    private static void showRateDialog(final Context context, final SharedPreferences.Editor preferencesEditor) {
-        dialog = new Dialog(context);
+    private static void showRateDialog(final Context context, SharedPreferences.Editor preferencesEditor) {
+        CharSequence rateAppDialogTitle = context.getText(R.string.rate);
 
-        CharSequence rateApp = context.getText(R.string.rate);
-        dialog.setTitle(rateApp);
+        TextView pleaseRateAppTextView = new TextView(context);
+        pleaseRateAppTextView.setText(context.getText(R.string.pleaseRateApp));
+        pleaseRateAppTextView.setTextAppearance(context, R.style.alertDialogColor);
+        int padding = 10;
+        int topPadding = 5;
+        pleaseRateAppTextView.setPadding(padding, topPadding, padding, padding);
 
         LinearLayout linearLayout = new LinearLayout(context);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
 
-        TextView pleaseRateAppTextView = new TextView(context);
-        pleaseRateAppTextView.setText(context.getText(R.string.pleaseRateApp));
-        pleaseRateAppTextView.setWidth(240);
-        pleaseRateAppTextView.setPadding(4, 0, 4, 10);
         linearLayout.addView(pleaseRateAppTextView);
+        linearLayout.addView(createRateAppButton(context, rateAppDialogTitle, preferencesEditor));
+        linearLayout.addView(createRemindMeLaterButton(context));
+        linearLayout.addView(createNoThanksButton(context, preferencesEditor));
 
-        Button rateButton = new Button(context);
-        rateButton.setText(rateApp);
-        rateButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent anIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + APP_PACKAGE_NAME));
+        s_dialog = new Dialog(context);
+        s_dialog.setTitle(rateAppDialogTitle);
+        s_dialog.setContentView(linearLayout);
+        s_dialog.show();
+    }
+
+    /**
+     * Create button which will open Android PlayStore to let user rate the application.
+     *
+     * @param context
+     *         Activity's context.
+     * @param rateApp
+     *         Button's label.
+     */
+    private static Button createRateAppButton(final Context context, CharSequence rateApp, final SharedPreferences.Editor preferencesEditor) {
+        Button rateAppButton = new Button(context);
+        rateAppButton.setText(rateApp);
+        rateAppButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View callerView) {
+                Log.i(TAG, "Starting Play Store to rate app.");
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + context.getApplicationContext().getPackageName()));
+
+                disableRateAppDialog(preferencesEditor);
+                GoogleAnalyticsTracker.getInstance().trackEvent(GOOGLE_ANALYTICS_PAGE_NAME, GOOGLE_ANALYTICS_EVENT_KEY, "Rate app", 0);
                 try {
-                    context.startActivity(anIntent);
+                    context.startActivity(intent);
                 } catch (ActivityNotFoundException activityNotFoundException) {
                     Log.e(TAG, "Can't open Play Store because it's not present on running device.");
                 }
 
-                dialog.dismiss();
+                s_dialog.dismiss();
             }
         });
-        linearLayout.addView(rateButton);
 
+        return rateAppButton;
+    }
+
+    /**
+     * Create a button that does nothing to remind the user later.
+     *
+     * @param context
+     *         Activity's context.
+     */
+    private static Button createRemindMeLaterButton(Context context) {
         Button remindMeLaterButton = new Button(context);
         remindMeLaterButton.setText(context.getText(R.string.remindMeLater));
         remindMeLaterButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                dialog.dismiss();
+            public void onClick(View callerView) {
+                Log.i(TAG, "User wants to let app remind him later about rating the application.");
+                GoogleAnalyticsTracker.getInstance().trackEvent(GOOGLE_ANALYTICS_PAGE_NAME, GOOGLE_ANALYTICS_EVENT_KEY, "Remind later", 0);
+                s_dialog.dismiss();
             }
         });
-        linearLayout.addView(remindMeLaterButton);
 
+        return remindMeLaterButton;
+    }
+
+    /**
+     * Create a button to disable this dialog for ever.
+     *
+     * @param context
+     *         Activity's context.
+     */
+    private static Button createNoThanksButton(Context context, final SharedPreferences.Editor preferencesEditor) {
         Button noThanksButton = new Button(context);
         noThanksButton.setText(context.getText(R.string.noThanks));
         noThanksButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (preferencesEditor != null) {
-                    preferencesEditor.putBoolean(DONT_SHOW_DIALOG_AGAIN, true);
-                    preferencesEditor.commit();
-                }
-                dialog.dismiss();
+            public void onClick(View callerView) {
+                Log.i(TAG, "User don't want to receive prompts to rate the application again.");
+                disableRateAppDialog(preferencesEditor);
+                GoogleAnalyticsTracker.getInstance().trackEvent(GOOGLE_ANALYTICS_PAGE_NAME, GOOGLE_ANALYTICS_EVENT_KEY, "Don't show again", 0);
+                s_dialog.dismiss();
             }
         });
-        linearLayout.addView(noThanksButton);
 
-        dialog.setContentView(linearLayout);
-        dialog.show();
+        return noThanksButton;
     }
+
+
+    /**
+     * Sets a flag to disable this dialog for ever.
+     *
+     * @param preferencesEditor
+     *         The SharedPreferences Editor used to store user preferences.
+     */
+    private static void disableRateAppDialog(SharedPreferences.Editor preferencesEditor) {
+        if (preferencesEditor != null) {
+            preferencesEditor.putBoolean(DONT_SHOW_DIALOG_AGAIN, true);
+            preferencesEditor.commit();
+            Log.i(TAG, "AppRater dialog was disabled.");
+        }
+    }
+
 }
 
